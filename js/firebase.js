b0a5ba92df673f685071afceb47bf5f444bc5b07
zezepequeno <rafaelaranja90@gmail.com>
2026-02-08 00:46:38 -0300

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,1 +1,22 @@
+// Configuração Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyCWFLa-oKekYvvEhaxL_S8NZopxI-GID60",
+  authDomain: "zeze-dos-dimas.firebaseapp.com",
+  projectId: "zeze-dos-dimas",
+  storageBucket: "zeze-dos-dimas.firebasestorage.app",
+  messagingSenderId: "831377776200",
+  appId: "1:831377776200:web:089b2ff4120db93325827d",
+  measurementId: "G-YNDH2FH765"
+};
 
+// Inicializa Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Serviços
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+// Força seleção de conta
+provider.setCustomParameters({
+  prompt: "select_account"
+});
