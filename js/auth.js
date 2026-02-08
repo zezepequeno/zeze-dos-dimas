995a783812cbd6ca4ff3fbc8ff141a66248c1f7f
zezepequeno <rafaelaranja90@gmail.com>
2026-02-08 00:47:02 -0300

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,1 +1,27 @@
+// Verifica autenticação
+auth.onAuthStateChanged(user => {
+  if (!user) {
+    loginComGoogle();
+  } else {
+    console.log("Usuário logado:", user.displayName);
+  }
+});
 
+// Função de login
+function loginComGoogle() {
+  auth.signInWithPopup(provider)
+    .then(result => {
+      console.log("Login realizado:", result.user.displayName);
+    })
+    .catch(error => {
+      console.error("Erro no login:", error);
+      alert("Erro ao fazer login com Google.");
+    });
+}
+
+// Logout (pronto pra usar depois)
+function logout() {
+  auth.signOut().then(() => {
+    location.reload();
+  });
+}
