cdcca05458ada3c9206f4c502595cfc24f058e45
zezepequeno <rafaelaranja90@gmail.com>
2026-02-08 13:17:28 -0300

    Delete js/admin-check.js

--- js/admin-check.js
+++
@@ -1,9 +0,0 @@
-const ADMIN_EMAIL = "virtualinvest@gmail.com";
-
-auth.onAuthStateChanged(user => {
-  const btnAdmin = document.getElementById("btnAdmin");
-  if (!btnAdmin) return;
-
-  btnAdmin.style.display =
-    user && user.email === ADMIN_EMAIL ? "inline-block" : "none";
-});
