034dcda499459d569ffa37adb7f1d5408784fb47
zezepequeno <rafaelaranja90@gmail.com>
2026-02-08 00:19:47 -0300

    Update roleta.js

--- js/roleta.js
+++ js/roleta.js
@@ -1,1 +1,15 @@
+function girarRoleta() {
+  const premios = [
+    "🎟 Cupom 5%",
+    "🎟 Cupom 10%",
+    "💎 Diamantes",
+    "🔥 Item Raro",
+    "❌ Não foi dessa vez"
+  ];
 
+  const sorteio = Math.floor(Math.random() * premios.length);
+  const resultado = premios[sorteio];
+
+  const resultadoEl = document.getElementById("resultadoRoleta");
+  resultadoEl.innerText = "🎉 Resultado: " + resultado;
+}
