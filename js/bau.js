e04976d33105a1cd031e103b663700517dc3e530
zezepequeno <rafaelaranja90@gmail.com>
2026-02-08 00:20:16 -0300

    Update bau.js

--- js/bau.js
+++ js/bau.js
@@ -1,1 +1,15 @@
+function abrirBau() {
+  const premios = [
+    "🎁 Cupom 15%",
+    "💎 100 Diamantes",
+    "🎫 Passe Elite",
+    "🔥 Skin Aleatória",
+    "❌ Baú vazio"
+  ];
 
+  const sorteio = Math.floor(Math.random() * premios.length);
+  const resultado = premios[sorteio];
+
+  const resultadoEl = document.getElementById("resultadoBau");
+  resultadoEl.innerText = "🎉 Você ganhou: " + resultado;
+}
